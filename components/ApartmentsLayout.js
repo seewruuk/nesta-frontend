1759a5527e58720e12e4ca78d7dbbda60d90828a
seewruuk <sewruczekk@icloud.com>
2025-05-04 16:15:55 +0200

    home page etc. p4

--- components/ApartmentsLayout.js
+++ components/ApartmentsLayout.js
@@ -57,11 +57,11 @@ export default function ApartmentsLayout() {
     useEffect(() => applyFilters(), [filters]);
 
     return (
-        <PageTransition>
+        <>
             <div className="flex bg-gray-50 min-h-screen items-start pt-[100px]">
                 <FilterSidebar filters={filters} setFilters={setFilters}/>
                 <ApartmentsList apartments={filteredApartments}/>
             </div>
-        </PageTransition>
+        </>
     );
 }
